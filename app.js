a22de104d317ba7c9cb93028a84ef7509819695d
Deep Patel <deep1907rp@gmail.com>
2023-10-16 23:32:50 +0530

    deploy

--- app.js
+++ app.js
@@ -17,13 +17,12 @@ const express = require('express');
 const app = express();
 dotenv.config({ path: './.env' });
 app.set('trust proxy', 1); // trust first proxy
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 const corsOptions ={
 	origin:["http://localhost:3000","https://filmfolio-git-temp2-dp-nothing.vercel.app"],
     credentials:true,            //access-control-allow-credentials:true
